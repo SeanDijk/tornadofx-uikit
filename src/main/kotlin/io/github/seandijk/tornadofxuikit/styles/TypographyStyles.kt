aaf6883a3b3c59cc984f8481b0a6e90d3fd004eb
Sean <seandijk@gmail.com>
2019-01-06 17:11:49 +0100

    Added typography styles

--- src/main/kotlin/io/github/seandijk/tornadofxuikit/styles/TypographyStyles.kt
+++ src/main/kotlin/io/github/seandijk/tornadofxuikit/styles/TypographyStyles.kt
@@ -1,15 +1,77 @@
 package io.github.seandijk.tornadofxuikit.styles
 
+import io.github.seandijk.tornadofxuikit.styles.UIKitStyles.Companion.ukTextDanger
+import io.github.seandijk.tornadofxuikit.styles.UIKitStyles.Companion.ukTextEmphasis
+import io.github.seandijk.tornadofxuikit.styles.UIKitStyles.Companion.ukTextLarge
+import io.github.seandijk.tornadofxuikit.styles.UIKitStyles.Companion.ukTextLead
+import io.github.seandijk.tornadofxuikit.styles.UIKitStyles.Companion.ukTextMeta
+import io.github.seandijk.tornadofxuikit.styles.UIKitStyles.Companion.ukTextMuted
+import io.github.seandijk.tornadofxuikit.styles.UIKitStyles.Companion.ukTextPrimary
+import io.github.seandijk.tornadofxuikit.styles.UIKitStyles.Companion.ukTextSmall
+import io.github.seandijk.tornadofxuikit.styles.UIKitStyles.Companion.ukTextSuccess
+import io.github.seandijk.tornadofxuikit.styles.UIKitStyles.Companion.ukTextWarning
 import javafx.scene.text.FontWeight
 import tornadofx.*
 
 
 class TypographyStyles: Stylesheet() {
     init {
-        label and UIKitStyles.heading {
-            padding = box(10.px)
-            fontSize = 20.px
-            fontWeight = FontWeight.BOLD
+
+        ukTextLead{
+            +Mixins.largeText
+            +Mixins.emphasis
+        }
+
+        ukTextMeta{
+            +Mixins.smallText
+            +Mixins.muted
+        }
+
+        ukTextSmall{
+            fontSize = 14.px
+            fontWeight = FontWeight.NORMAL
+        }
+        ukTextLarge{
+            +Mixins.largeText
+        }
+
+
+
+        //Text color
+        ukTextMuted {
+            +Mixins.muted
+        }
+        ukTextEmphasis {
+            +Mixins.emphasis
+        }
+        ukTextPrimary {
+            textFill = c("#1e87f0")
+        }
+        ukTextSuccess {
+            textFill = c("#32d296")
+        }
+        ukTextWarning {
+            textFill = c("#faa05a")
+        }
+        ukTextDanger {
+            textFill = c("#f0506e")
+        }
+    }
+
+    private object Mixins{
+        val largeText = mixin {
+            fontSize = 24.px
+            fontWeight = FontWeight.LIGHT
+        }
+        val smallText = mixin {
+            fontSize = 24.px
+            fontWeight = FontWeight.LIGHT
+        }
+        val emphasis = mixin {
+            textFill = c("#222")
+        }
+        val muted = mixin {
+            textFill = c("#999")
         }
     }
 }
