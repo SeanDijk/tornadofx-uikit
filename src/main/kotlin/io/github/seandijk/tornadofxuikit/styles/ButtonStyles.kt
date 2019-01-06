262024b13322e13f931f0205779814008cd752ed
Sean <seandijk@gmail.com>
2019-01-06 17:10:14 +0100

    Refactored buttons

--- src/main/kotlin/io/github/seandijk/tornadofxuikit/styles/ButtonStyles.kt
+++ src/main/kotlin/io/github/seandijk/tornadofxuikit/styles/ButtonStyles.kt
@@ -11,75 +11,92 @@ import tornadofx.*
 
 class ButtonStyles:  Stylesheet() {
     init {
-        val ukButtonBase = mixin {
-            +UIKitStyles.Mixins.rectangular
-            UIKitStyles.montserratExtraBold?.let { font = it ; fontFamily = it.family;}
-
-            borderWidth += box(1.px)
-            padding = box(10.px, 30.px)
-//            //Make rectangular
-//            backgroundRadius = multi(box(Dimension(0.0, Dimension.LinearUnits.em)))
-//            borderRadius = multi(tornadofx.box(0.0.em))
-
-        }
-        class UkButtonFilled(color: Color, textColor: Color){
-            val darker10p = darkenColor(color)
-            val darker20p = darkenColor(darker10p)
-
-            val myMixin = mixin {
-                +ukButtonBase
-                borderColor += box(Color.TRANSPARENT)
-                backgroundColor = multi(color)
-                color.darker()
-                textFill = textColor
-
-                and(Stylesheet.hover){
-                    backgroundColor = multi(darker10p)
-                }
-                and(Stylesheet.pressed){
-                    backgroundColor = multi(darker20p)
-                }
-            }
-        }
-
-
         ukButtonDefault {
-            + ukButtonBase
-            borderColor += box(Color.web("e5e5e5"))
-            backgroundColor = multi(Color.WHITE)
-            textFill = Color.BLACK
-
-            and(Stylesheet.hover){
-                borderColor += box(Color.DARKGRAY)
-            }
-            and(Stylesheet.pressed){
-                borderColor += box(Color.BLACK)
-            }
+            +Mixins.createButtonMixin(
+                    buttonColor = Color.WHITE,
+                    borderColor = Color.web("e5e5e5"),
+                    borderHover = Color.DARKGRAY,
+                    borderClick = Color.BLACK,
+                    textColor = Color.BLACK)
         }
 
         ukButtonPrimary{
-            + ukButtonBase
-            borderColor += box(Color.TRANSPARENT)
-            backgroundColor = multi(UIKitStyles.COLOR_PRIMARY)
-            textFill = Color.WHITE
-
-            and(Stylesheet.hover){
-                backgroundColor = multi(UIKitStyles.COLOR_PRIMARY_10pDarker)
-            }
-            and(Stylesheet.pressed){
-                backgroundColor = multi(UIKitStyles.COLOR_PRIMARY_20pDarker)
-            }
+            +Mixins.createButtonMixin(
+                    buttonColor = UIKitStyles.COLOR_PRIMARY,
+                    buttonHover = darkenColor(UIKitStyles.COLOR_PRIMARY),
+                    buttonClick = darkenColor(UIKitStyles.COLOR_PRIMARY, 2),
+                    borderColor = Color.TRANSPARENT,
+                    textColor = Color.WHITE
+            )
         }
-
 
         ukButtonSecondary{
-            +UkButtonFilled(UIKitStyles.COLOR_PRIMARY, Color.WHITE).myMixin
+            +Mixins.createButtonMixin(
+                    buttonColor = UIKitStyles.COLOR_PRIMARY,
+                    buttonHover = UIKitStyles.COLOR_PRIMARY.darker(),
+                    buttonClick = UIKitStyles.COLOR_PRIMARY.darker().darker(),
+                    borderColor = Color.TRANSPARENT,
+                    textColor = Color.WHITE
+            )
         }
         ukButtonDanger{
-            +UkButtonFilled(UIKitStyles.COLOR_DANGER, Color.WHITE).myMixin
+            +Mixins.createButtonMixin(
+                    buttonColor = UIKitStyles.COLOR_DANGER,
+                    buttonHover = darkenColor(UIKitStyles.COLOR_DANGER),
+                    buttonClick = darkenColor(UIKitStyles.COLOR_DANGER, 2),
+                    borderColor = Color.TRANSPARENT,
+                    textColor = Color.WHITE
+            )
         }
         ukButtonPositive{
-            +UkButtonFilled(UIKitStyles.COLOR_POSITIVE, Color.WHITE).myMixin
+            +Mixins.createButtonMixin(
+                    buttonColor = UIKitStyles.COLOR_POSITIVE,
+                    buttonHover = darkenColor(UIKitStyles.COLOR_POSITIVE),
+                    buttonClick = darkenColor(UIKitStyles.COLOR_POSITIVE, 2),
+                    borderColor = Color.TRANSPARENT,
+                    textColor = Color.WHITE
+            )
+        }
+    }
+
+    private object Mixins{
+
+        fun createButtonMixin(buttonColor: Color,
+                              buttonHover: Color = buttonColor,
+                              buttonClick: Color = buttonColor,
+                              borderColor: Color,
+                              borderHover: Color = borderColor,
+                              borderClick: Color = borderColor,
+                              textColor: Color,
+                              textHover: Color = textColor,
+                              textClick: Color = textColor): CssSelectionBlock {
+            return mixin {
+                //Set shape and font
+                +UIKitStyles.Mixins.rectangular
+                UIKitStyles.montserratExtraBold?.let { font = it ; fontFamily = it.family;}
+
+                borderWidth += box(1.px)
+                padding = box(10.px, 30.px)
+
+                //Set the default colors
+                this.backgroundColor = multi(buttonColor)
+                this.borderColor += box(borderColor)
+                this.textFill = textColor
+
+                //Hover colors
+                and(Stylesheet.hover){
+                    this.backgroundColor = multi(buttonHover)
+                    this.borderColor += box(borderHover)
+                    this.textFill = textHover
+                }
+
+                //Click colors
+                and(Stylesheet.pressed){
+                    this.backgroundColor = multi(buttonClick)
+                    this.borderColor += box(borderClick)
+                    this.textFill = textClick
+                }
+            }
         }
     }
 }
